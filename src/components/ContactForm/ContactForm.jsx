e176fd91ac0d956f089b617b3e5e5759be4866b3
Анастасия Маслак <96750008+AnastasiiaKim1996@users.noreply.github.com>
2022-10-23 19:54:28 +0300

    refact/add hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,74 +1,142 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { Form, Label, Input, BtnWrapper, Button } from './ContactForm.styled';
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   // записуємо id у змінні щоб у елементів змінної було однакове id для взаємодії //
 
-  nameId = nanoid();
-  numberId = nanoid();
+  const nameId = nanoid();
+  const numberId = nanoid();
 
   // метод, який витягує з інпутів їх значення і записує у стейт  //
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    if (name === 'name') {
+      setName(value);
+    }
+
+    if (name === 'number') {
+      setNumber(value);
+    }
   };
 
   // метод для відправлення форми, формує контакти зі стейту та передає до зовнішнього методу //
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { name, number } = this.state;
-    this.props.onSubmit({ name, number });
-    this.setState({
-      name: '',
-      number: '',
-    });
+    const newContact = {
+      id: nanoid(),
+      name: e.currentTarget.name.value,
+      number: e.currentTarget.number.value,
+    };
+    onSubmit(newContact);
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { nameId, numberId } = this;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label htmlFor={nameId}> Name </Label>
-        <Input
-          type="text"
-          id={nameId}
-          name="name"
-          value={this.state.name} // записуємо значення до стейту //
-          onChange={this.handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <Label htmlFor={numberId}> Contacts</Label>
-        <Input
-          type="tel"
-          id={numberId}
-          name="number"
-          value={this.state.number} // записуємо значення до стейту //
-          onChange={this.handleChange}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-        <BtnWrapper>
-          <Button type="submit">Add contact</Button>
-        </BtnWrapper>
-      </Form>
-    );
-  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor={nameId}> Name </Label>
+      <Input
+        type="text"
+        id={nameId}
+        name="name"
+        value={name} // записуємо значення до стейту //
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <Label htmlFor={numberId}> Contacts</Label>
+      <Input
+        type="tel"
+        id={numberId}
+        name="number"
+        value={number} // записуємо значення до стейту //
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <BtnWrapper>
+        <Button type="submit">Add contact</Button>
+      </BtnWrapper>
+    </Form>
+  );
 }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// export class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   // записуємо id у змінні щоб у елементів змінної було однакове id для взаємодії //
+
+//   nameId = nanoid();
+//   numberId = nanoid();
+
+//   // метод, який витягує з інпутів їх значення і записує у стейт  //
+
+//   handleChange = e => {
+//     const { name, value } = e.target;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   // метод для відправлення форми, формує контакти зі стейту та передає до зовнішнього методу //
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { name, number } = this.state;
+//     this.props.onSubmit({ name, number });
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//   };
+
+//   render() {
+//     const { nameId, numberId } = this;
+//     return (
+//       <Form onSubmit={this.handleSubmit}>
+//         <Label htmlFor={nameId}> Name </Label>
+//         <Input
+//           type="text"
+//           id={nameId}
+//           name="name"
+//           value={this.state.name} // записуємо значення до стейту //
+//           onChange={this.handleChange}
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//           required
+//         />
+//         <Label htmlFor={numberId}> Contacts</Label>
+//         <Input
+//           type="tel"
+//           id={numberId}
+//           name="number"
+//           value={this.state.number} // записуємо значення до стейту //
+//           onChange={this.handleChange}
+//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//           required
+//         />
+//         <BtnWrapper>
+//           <Button type="submit">Add contact</Button>
+//         </BtnWrapper>
+//       </Form>
+//     );
+//   }
+// }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,78 +1,57 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-
 import { Container, Title } from './Form.styled';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 
-export class Form extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export function Form() {
+  // const [contacts, setContacts] = useState(
+  //   JSON.parse(localStorage.getItem('contacts')) ?? [] // LL ліниве завантаження //
+  // );
+  const [contacts, setContacts] = useState(() => {
+    const value = JSON.parse(localStorage.getItem('contacts'));
+    return value ?? [];
+  });
+  const [filter, setFilter] = useState('');
 
-  // методи життєвого циклу //
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidMount() {
-    console.log('Phonebook componentDidMount');
-
-    const contact = localStorage.getItem('contacts');
-    const parsedContact = JSON.parse(contact);
-    if (parsedContact) {
-      this.setState({ contacts: parsedContact });
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    console.log('Phonebook componentDidUpdate');
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    return () => {
+      localStorage.removeItem('contacts');
+    };
+  }, []);
 
   // метод додавання нових контактів//
 
-  addContact = contact => {
-    const ContactChecked = this.state.contacts.find(
-      data => data.name === contact.name
-    );
+  const addContact = contact => {
+    const ContactChecked = contacts.find(data => data.name === contact.name);
     ContactChecked
       ? window.alert(`${contact.name} is already in contacts 😉`)
-      : this.setState(prev => {
+      : setContacts(prev => {
           const newContact = {
             id: nanoid(),
             ...contact,
           };
-          return {
-            contacts: [...prev.contacts, newContact],
-          };
+          return [...prev, newContact];
         });
   };
 
-  removeContact = id => {
-    this.setState(prevState => {
-      const newContacts = prevState.contacts.filter(
-        contact => contact.id !== id
-      );
-      return {
-        contacts: newContacts,
-      };
-    });
+  const removeContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+  const handleChange = e => {
+    const { value } = e.target;
+    setFilter(value);
   };
 
   // метод фільтрації контактів //
 
-  filteredContacts() {
-    const { contacts, filter } = this.state;
-
+  const filteredContacts = () => {
     //при умові якщо фільтр порожній то повертається масив контактів //
 
     if (!filter) {
@@ -82,21 +61,110 @@ export class Form extends Component {
     return contacts.filter(data =>
       data.name.toUpperCase().includes(normalizedFilter)
     );
-  }
-
-  render() {
-    console.log('Phonebook render');
-    const { filter } = this.state;
-    const contacts = this.filteredContacts();
-    return (
-      <Container>
-        <Title>Phonebook</Title>
-
-        <ContactForm onSubmit={this.addContact} />
-        <Title>Contacts</Title>
-        <Filter value={filter} onChange={this.handleChange} />
-        <ContactList items={contacts} removeContact={this.removeContact} />
-      </Container>
-    );
-  }
+  };
+  const filterContacts = filteredContacts();
+  return (
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm onSubmit={addContact} />
+      <Title>Contacts</Title>
+      <Filter value={filter} onChange={handleChange} />
+      <ContactList items={filterContacts} removeContact={removeContact} />
+    </Container>
+  );
 }
+
+// export class Form extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//   // методи життєвого циклу //
+
+//   componentDidMount() {
+//     console.log('Phonebook componentDidMount');
+
+//     const contact = localStorage.getItem('contacts');
+//     const parsedContact = JSON.parse(contact);
+//     if (parsedContact) {
+//       this.setState({ contacts: parsedContact });
+//     }
+//   }
+
+//   componentDidUpdate(prevState) {
+//     console.log('Phonebook componentDidUpdate');
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   // метод додавання нових контактів//
+
+//   addContact = contact => {
+//     const ContactChecked = this.state.contacts.find(
+//       data => data.name === contact.name
+//     );
+//     ContactChecked
+//       ? window.alert(`${contact.name} is already in contacts 😉`)
+//       : this.setState(prev => {
+//           const newContact = {
+//             id: nanoid(),
+//             ...contact,
+//           };
+//           return {
+//             contacts: [...prev.contacts, newContact],
+//           };
+//         });
+//   };
+
+//   removeContact = id => {
+//     this.setState(prevState => {
+//       const newContacts = prevState.contacts.filter(
+//         contact => contact.id !== id
+//       );
+//       return {
+//         contacts: newContacts,
+//       };
+//     });
+//   };
+
+//   handleChange = e => {
+//     const { name, value } = e.target;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   // метод фільтрації контактів //
+
+//   filteredContacts() {
+//     const { contacts, filter } = this.state;
+
+//     //при умові якщо фільтр порожній то повертається масив контактів //
+
+//     if (!filter) {
+//       return contacts;
+//     }
+//     const normalizedFilter = filter.toUpperCase();
+//     return contacts.filter(data =>
+//       data.name.toUpperCase().includes(normalizedFilter)
+//     );
+//   }
+
+//   render() {
+//     console.log('Phonebook render');
+//     const { filter } = this.state;
+//     const contacts = this.filteredContacts();
+//     return (
+//       <Container>
+//         <Title>Phonebook</Title>
+
+//         <ContactForm onSubmit={this.addContact} />
+//         <Title>Contacts</Title>
+//         <Filter value={filter} onChange={this.handleChange} />
+//         <ContactList items={contacts} removeContact={this.removeContact} />
+//       </Container>
+//     );
+//   }
+// }
